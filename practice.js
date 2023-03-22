1153773fbcc464396138fc7f3df8cb7bdc8dc184
VKMALAVATH <vinodkumarmalavath21@gmail.com>
2023-03-22 01:16:53 -0500

    commit stringLength function

--- practice.js
+++ practice.js
@@ -4,3 +4,11 @@ function addNumber(num1, num2){
     return num1+num2;
 }
 console.log("result",addNumber(1100,1111));
+
+// Write a function that takes a string as input and returns the length of the string.
+  
+function stringLength(string){
+    return string.length;
+
+}
+console.log("string length is",stringLength("biigfdthfasdjkldsjk"));
