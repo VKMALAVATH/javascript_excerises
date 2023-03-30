de2d78f9c6855cd60cace6d02889aba2acd7fb24
VKMALAVATH <vinodkumarmalavath21@gmail.com>
2023-03-29 19:36:51 -0500

    cube

---
+++ cube.js
@@ -0,0 +1,13 @@
+function cube(num){
+    if (typeof num== 'number'){
+        return num*num*num;
+    }
+   
+
+}
+return 0;
+var output = cube ("vinod")
+var output1 = cube(8);
+console.log("output")
+console.log("output1")
+
