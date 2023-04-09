95ceddf064ca638f65692aea31f5e80ed11ed6af
VKMALAVATH <vinodkumarmalavath21@gmail.com>
2023-04-08 19:46:49 -0500

    getLengthOfWords

--- class3.js
+++ class3.js
@@ -37,9 +37,13 @@ console.log(computeSquareRoot(9))
 // Given 3 words, "getLengthOfThreeWords" returns the sum of their lengths.
 // var output = getLengthOfThreeWords('some', 'other', 'words');
 // console.log(output); // --> 14
+function getLengthOfThreeWords(word1,word2,word3){
+    return word1.length+word2.length+word3.length
+}
+console.log(getLengthOfThreeWords('some', 'other', 'words'))
+
 
 // Write a function to return substr of a string given a string with a given length
-
 // console.log(getSubStr(“badminton”,3))
 
 // Write a function called "computeTripledAreaOfARectangle".
